0134cd2257e43342a5751d2e47aa9228fb3ba804
Kevin Bui <kevinbui722@gmail.com>
2026-03-20 21:23:52 -0400

    Enhance IUserDB with party and PvP methods
    Added methods for party management and PvP results.

--- src/ProfileCreationSystem/IUserDB.java
+++ src/ProfileCreationSystem/IUserDB.java
@@ -1,7 +1,13 @@
-package ProfileCreationSystem;
+package Profile;
+
+import java.util.List;
 
 public interface IUserDB {
     boolean usernameExists(String username);
     void save(UserProfile user);
     UserProfile findUsername(String username);
+
+    boolean hasSavedParty(String username);
+    List<String> getSavedParties(String username);
+    void recordPvpResult(String winnerUsername, String loserUsername);
 }
