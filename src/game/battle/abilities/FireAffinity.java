0ce711b03501d29a6020d7a12f32bad7074f4726
Kevin Bui <kevinbui722@gmail.com>
2026-03-25 16:11:22 -0400

    Delete src/game/battle/abilities/FireAffinity.java

--- src/game/battle/abilities/FireAffinity.java
+++
@@ -1,8 +0,0 @@
-package game.battle.abilities;
-import game.battle.Hero;
-/** Decorator: grants +5 attack (fire affinity). */
-public class FireAffinity extends HeroDecorator {
-    public FireAffinity(Hero inner) { super(inner); }
-    @Override public int getAttack()  { return inner.getAttack() + 5; }
-    @Override public int getDefense() { return inner.getDefense(); }
-}
